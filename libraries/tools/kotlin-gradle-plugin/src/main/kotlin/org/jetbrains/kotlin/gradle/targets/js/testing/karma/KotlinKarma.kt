f768dbfebb1a264d651002b00b74ebeedcf0f1e2
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-04-09 21:02:45 +0300

    [Gradle, JS] All karma output to debug

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/testing/karma/KotlinKarma.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/testing/karma/KotlinKarma.kt
@@ -410,8 +410,7 @@ class KotlinKarma(override val compilation: KotlinJsCompilation) :
                 object : JSServiceMessagesClient(
                     testResultProcessor,
                     clientSettings,
-                    log,
-                    suppressedOutput
+                    log
                 ) {
                     val baseTestNameSuffix get() = settings.testNameSuffix
                     override var testNameSuffix: String? = baseTestNameSuffix
@@ -423,10 +422,7 @@ class KotlinKarma(override val compilation: KotlinJsCompilation) :
                         progressLogger.progress(value)
 
                         parseConsole(value)
-
-                        if (log.isDebugEnabled) {
-                            super.printNonTestOutput(text)
-                        }
+                        super.printNonTestOutput(text)
                     }
 
                     private fun parseConsole(text: String) {
